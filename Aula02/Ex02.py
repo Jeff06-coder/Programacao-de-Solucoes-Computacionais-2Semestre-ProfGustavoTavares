49d33d54198ff2798cbfb48e9bd1c5bbb8f39d16
Jeff06-coder <loplopado67@gmail.com>
2025-08-28 23:40:24 -0300

    Usando repetidores, for e while (teste)

---
+++ Aula02/Ex02.py
@@ -0,0 +1,11 @@
+#Fazendo uma repetição
+
+#for=para, in range=quantas vezez/quantidade e o i="valor"
+for i in range(5):
+    print("Contator:", i)
+
+contador = 0
+#while=enquanto (repete enquanto é verdadeiro)
+while contador < 5:
+  print("Enquanto: ", contador)
+contador +=1
